c85a8d3bc139472013b4814c0d77dbaff1057472
D288877 <D288877@edu.curio.nl>
2023-09-07 10:31:51 +0200

    Login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,17 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Admin Test',
-            'email' => 'Admin@example.com',
+            'name' => 'Teacher Test',
+            'email' => 'Teacher@example.com',
+            'password' => '123456',
             'is_admin' => '1'
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Student Test',
+            'email' => 'Student@example.com',
+            'password' => '12345678',
+            'is_admin' => '0'
         ]);
     }
 }
